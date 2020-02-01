0c31dc167abf1c53d726278ff21b142b846c70d5
CRStromberg <cstromberg86@gmail.com>
2020-01-31 18:32:34 -0700

    a.

--- ls/gui.py
+++ ls/gui.py
@@ -1,52 +1,28 @@
-import tkinter as tk 
-from tkinter import filedialog, Text
-import os
+import tkinter as tk
+#import os
+from pathlib import Path
+import glob
 
-root = tk.Tk()
-apps = []
+win = tk.Tk()
+win.title("LS GUI")
+win.geometry('475x275')
+win.configure(bg='#292929')
 
+types = ('*.*')
 
-def addApp():
+p = Path('/home/chris')
+filels = p.iterdir()
 
-    for widget in frame.winfo_children():
-        widget.destroy()
+files_grabbed = []
+for files in types:
+    files_grabbed.extend(glob.glob(files))
 
-    filename = filedialog.askopenfilename(initialdir="/", title="Select file", filetypes=((".exe", "*.exe"), ("all files", "*.*")))
-    apps.append(filename)
-    print(filename)
-    for app in apps:
-        label = tk.Label(frame, text=app, bg="gray")
-        label.pack()
+#filels = os.listdir('/')
 
-def runApps():
-    for app in apps:
-        os.startfile(app)
+lbox = tk.Listbox(win, width=60, height=40, bg='#292929', fg='#dfdfdf')
+lbox.pack()
 
-def list_files(startpath):
-    for root, dirs, files in os.walk(startpath):
-        level = root.replace(startpath, '').count(os.sep)
-        indent = ' ' * 4 * (level)
-        print('{}{}/'.format(indent, os.path.basename(root)))
-        subindent = ' ' * 4 * (level + 1)
-        for f in files:
-            print('{}{}'.format(subindent, f))
-
-canvas = tk.Canvas(root, height=500, width=500, bg="#263D42")
-canvas.pack()
-
-frame = tk.Frame(root, bg="white")
-frame.place(relwidth=0.8, relheight=0.8, relx=0.1, rely=0.1)
-
-#openFile = tk.Button(root, text="Open File", padx=10, pady=5, fg="white", bg="#263D42", command=addApp)
-#openFile.pack()
-
-<<<<<<< HEAD
-#runApps = tk.Button(root, text="Run Apps", padx=10, pady=5, fg="white", bg="#263D42")
-#runApps.pack()
-command = list_files
-=======
-runApps = tk.Button(root, text="Run Apps", padx=10, pady=5, fg="white", bg="#263D42", command=runApps)
-runApps.pack()
->>>>>>> 04636a09327afa726d7e2b287b8ccd5c12dc0649
-
-root.mainloop()
+for item in files_grabbed:
+    lbox.insert(tk.END, item)
+ 
+win.mainloop()
