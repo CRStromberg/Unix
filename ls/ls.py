d84fe798ecfdb02f5edc780c4d74ed915ea69ad0
CRStromberg <cstromberg86@gmail.com>
2020-02-07 10:41:20 -0700

    .

--- ls/ls.py
+++ ls/ls.py
@@ -3,7 +3,8 @@ import os, sys
 from stat import *
   
 def lssearch():
-    filels.delete(0,END)
+    filels.delete(0, END)
+   
     entdir=lsentry.get()
     file_search(entdir)
 
@@ -15,25 +16,32 @@ def file_search(temp):
 
     for file in dirs:
         print(file)
-        print(os.stat(user_path + file).st_size)
-        print(os.stat(user_path + file))
+        filestat = os.stat(user_path + file)
+        perms = filemode(filestat.st_mode)
+        filesize = filestat.st_size
+        info = [file,filesize,perms]
+        print(filesize)
+        print(perms)
+        filels.insert(END, info)
+       
+
 
 win = Tk()
 win.title("LS GUI")
 win.configure(background="#212121")
 
-Label(win, text="Enter Directory (/ for root): ", bg="#212121", fg="#059c0f").grid(row=0, column=0)
+Label(win, text="Enter Directory (/ for root): ", bg="#212121", fg="#059c0f", font='bold').grid(row=0, column=0)
 
 lsentry = Entry(win, width=15, bg="white")
-lsentry.grid(row=0, column=2)
+lsentry.grid(row=1, column=0)
 
-Button(win, text="Search", bg="#7a7a7a", fg="Black", command=lssearch).grid(row=0, column=3)
+Button(win, text="Search", bg="#7a7a7a", fg="Black", command=lssearch).grid(row=2, column=0)
+
+Label(win, text="Name, Bytes, Permissions", bg="#212121", fg="#059c0f", width=40, font='bold').grid(row=5, column=0)
 
 
-filels = Listbox(win, bg="#212121", fg="#059c0f")
-filels.grid(row=3, column=0)
-sizels = Listbox(win, bg="#212121", fg="#059c0f")
-sizels.grid(row=3, column=5)
+filels = Listbox(win, bg="#212121", fg="#059c0f", height=20, width=40)
+filels.grid(row=7, column=0)
 
 
 mainloop() 
